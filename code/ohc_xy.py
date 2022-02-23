70865630a016be5ffe8a1b89c0c83440e9e0d8a4
emmomp <emmomp@bas.ac.uk>
2022-02-23 12:13:01 +0000

    Cleaning up ohc_xy code

---
+++ code/ohc_xy.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ohc_xy.py
+
+Code to load ocean temperature data from the SMURPHS ensemble model output and 
+calculate time series of depth-integrated Ocean Heat Content from 1955-2014. 
+
+For the SMURPHS ensemble, see (See Dittus et al. 2020 https://doi.org/10.1029/2019GL085806) 
+and for the CMIP6 historical ensemble, Andrews et al. (2020) https://doi.org/10.1029/2019MS001995.
+
+See https://github.com/emmomp/SMURPHS_OHC for details
+
+Created on Wed Dec  2 12:19:10 2020
+
+@author: emmomp
+"""
+
+import baspy as bp
+import xarray as xr
+import date_range
+from datetime import date
+
+rho_0 = 1.027e3 
+c_p = 3850
+
+attrs={'contact':'emmomp@bas.ac.uk',
+       'references':'SMURPHS OHC data from Boland et al (in prep)',
+       'date':'Created on '+date.today().strftime("%d/%m/%Y"),
+       'notes':'Data produced by analysis of the SMURPHS ensemble, See Dittus et al. 2020 https://doi.org/10.1029/2019GL085806'}
+
+save_dir = '../data_in/' #Directory to save data to
+
+griddata = xr.open_dataset(save_dir+'model_other_data/nemo_at491o_1m_19991201-20000101_grid-T.nc')
+
+depthlabels=['0-700m','0-2000m']
+depthbins = [[0,701],[0,2001]]
+nd = len(depthbins)
+
+# Generate dataframe with all temp data
+df = bp.catalogue(dataset='smurphs',Var=['thetao'])
+nt = len(bp.get_files(df.iloc[0]))
+
+exps = set(df.Experiment)
+runs = set(df.RunID)
+
+fname_struct='Var_Model_Experiment_RunID_Frequency_StartDate-EndDate_grid'
+startdate=19550101
+enddate=20141231
+
+def preproc_data(ds):
+    ds=ds.swap_dims({'time_counter':'time_centered'})
+    return ds.drop('deptht_bounds')
+
+for index, row in df.iterrows():
+    exp = row[0]
+    run = row[1]
+    print('Opening row '+exp+' '+run)
+    files = bp.get_files(row)
+    # Pull out 1955 onwards
+    sfiles = date_range.get_date_range_files(fname_struct,files,startdate,enddate)
+    print('Found ',len(sfiles),' files ')
+
+    with xr.open_mfdataset(sfiles,preprocess=preproc_data,concat_dim='time_centered',combine='nested',data_vars='minimal', coords='minimal', compat='override') as data:
+        print('Got data, calculating ohc ')
+        data_weighted = data.thetao*griddata.thkcello.squeeze(drop=True)
+        ohc_xy=data_weighted.sum(dim='deptht')*rho_0*c_p
+        ohc_xy['exp']=exp
+        ohc_xy['run']=run
+        ohc_xy.name='ohc'
+        ohc_xy.attrs['long_name']='Ocean Heat Content, full depth integrated'
+        ohc_xy.attrs['units']='J/m^2'   
+        ohc_xy.attrs.update(attrs)   
+        
+        ohc_xy_bybins=[]
+        for s in range(0,2):
+            foo=data_weighted.sel(deptht=slice(depthbins[s][0],depthbins[s][1])).sum(dim='deptht')*rho_0*c_p
+            foo['depth_range']=depthlabels[s]
+            ohc_xy_bybins.append(foo)
+        ohc_xy_bybins=xr.concat(ohc_xy_bybins,'depth_range')  
+        ohc_xy_bybins['exp']=exp
+        ohc_xy_bybins['run']=run
+        ohc_xy_bybinsname='ohc'
+        ohc_xy_bybins.attrs['long_name']='Heat content, depth integrated'
+        ohc_xy_bybins.attrs['units']='J/m^2'   
+        ohc_xy_bybins.attrs.update(attrs)     
+   
+        ohc_xy.ohc.to_netcdf(save_dir+'ohc_xy_'+exp+'_'+run+'_'+str(startdate)+'_'+str(enddate)+'.nc')
+        ohc_xy_bybins.ohc.to_netcdf(save_dir+'ohc_xy_bydepth_'+exp+'_'+run+'_'+str(startdate)+'_'+str(enddate)+'.nc')
+
+print('All done')
+    
---
+++ code/ohc_xy_pic.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ohc_xy_pic.py
+
+Code to load ocean temperature data from the PIC of HadEM3-GC31-LL
+calculate time series of depth-integrated Ocean Heat Content for de-drifting
+SMURPHS ensemble. 
+
+For the SMURPHS ensemble, see (See Dittus et al. 2020 https://doi.org/10.1029/2019GL085806) 
+and for the CMIP6 historical ensemble, Andrews et al. (2020) https://doi.org/10.1029/2019MS001995.
+
+See https://github.com/emmomp/SMURPHS_OHC for details
+
+Created on Wed Nov 24 12:27:40 2021
+
+@author: emmomp
+"""
+from datetime import date
+import baspy as bp
+
+rho_0 = 1.027e3 
+c_p = 3850
+
+attrs={'contact':'emmomp@bas.ac.uk',
+       'references':'HadGem3-GC31-LL PIC OHC data from Boland et al (in prep)',
+       'date':'Created on '+date.today().strftime("%d/%m/%Y"),
+       'notes':'Data produced by analysis of HadGem3-GC31-LL CMIP6 PIC data, Andrews et al. (2020) https://doi.org/10.1029/2019MS001995'}
+
+save_dir = '../data_in/' #Directory to save data to
+
+df = bp.catalogue(dataset='cmip6',Var=['thkcello'],Model='HadGEM3-GC31-LL',Experiment='piControl')
+data=bp.open_dataset(df)
+dz=data.thkcello
+
+df = bp.catalogue(dataset='cmip6',Var=['thetao'],Model='HadGEM3-GC31-LL',Experiment='piControl')
+data= bp.open_dataset(df)
+
+print('Got data, calculating ohc ')
+data_weighted = data.thetao*dz.squeeze(drop=True)
+ohc_xy=data_weighted.sum(dim='lev')*rho_0*c_p
+ohc_xy.name='ohc'
+ohc_xy.attrs['long_name']='Ocean Heat Content, full depth integrated'
+ohc_xy.attrs['units']='J/m^2'     
+   
+print('writing to file')
+ohc_xy.to_netcdf(save_dir+'ohc_xy_pic.nc')
+    
+print('All done')
