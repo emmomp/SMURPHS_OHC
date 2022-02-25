61e37e369668196677250ed9860604b4629e87f7
emmomp <emmomp@bas.ac.uk>
2022-02-25 21:00:54 +0000

    added date_range fns to utils

--- code/utils.py
+++ code/utils.py
@@ -10,6 +10,7 @@ import numpy as np
 import xarray as xr
 from scipy import stats
 import statsmodels.api as sm
+import os
 
 ##Create 4th order Bworth filter
 def butter_lowpass(data,cut,order=4,sample_freq=1) :
@@ -103,4 +104,71 @@ def polyval_xr(model,x):
     )
     return fit
 
+# From Scott Hosking github.com/scotthosking/baspy/examples/
+def get_file_date_ranges(fnames, filename_structure):
+    ### Get start and end dates from file names
+    ind = filename_structure.split('_').index('StartDate-EndDate')
+    start_dates, end_dates = np.array([]), np.array([])
+
+    for fname in list(fnames):
+
+        fname = os.path.splitext(fname)[0] # rm extention
+
+        ### Is this file time-varying?
+        if '_fx_' in fname:
+            ### Fixed variable (e.g., land-mask)
+            start_date, end_date = 0, 0
+
+        else:
+            ### Time-varying (e.g., temperature)
+            date_str = fname.split('_')[ind].split('-')
+
+            ### Interpreting the dates
+            if len(date_str) == 2:
+                ### e.g.,'19900101-20000101'
+                start_date, end_date = int(date_str[0]), int(date_str[1])
+
+            elif (len(date_str) == 3):
+                if (date_str[2] == 'clim'):
+                    ### e.g.,'186001-187912-clim'
+                    ### see /badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/piControl/mon/atmos/Amon/r1i1p1/latest/pfull
+                    start_date, end_date = int(date_str[0]+'01'), int(date_str[1]+'31')
+                else:
+                    print('Cannot identify dates '+fname)
+
+            elif (len(date_str) == 1) & (int(date_str[0]) >= 1800) & (int(date_str[0]) <= 2300):
+                ### e.g., '1990'
+                ### see /badc/cmip5/data/cmip5/output1/ICHEC/EC-EARTH/amip/subhr/atmos/cfSites/r3i1p1/latest/ccb
+                start_date, end_date = int(date_str[0]), int(date_str[0])
+
+            else:
+                ### Can't define date
+                ###### To do: if no date_range then get from ncdump? !!
+                print('Cannot identify dates '+fname)
+                start_date, end_date = np.nan, np.nan
+
+        start_dates = np.append( start_dates, start_date )
+        end_dates   = np.append( end_dates,   end_date )   
+
+    return start_dates, end_dates
+
+def get_date_range_files(filename_structure,netcdfs,start_date,end_date):
+
+    ### List file names that appear to be within our date range
+    if (start_date != None) | (end_date != None):
+        file_date_range = get_file_date_ranges(netcdfs, filename_structure)
+        if (start_date == None): start_date = np.min(file_date_range[0])
+        if (end_date == None):   end_date   = np.max(file_date_range[1])
+    
+        #keep_ind1 = np.where( (end_date >= file_date_range[0])   & (end_date <= file_date_range[1])   )[0]
+        keep_ind1 = np.where( (end_date >= file_date_range[0])   & (start_date <= file_date_range[0])   )[0]
+        keep_ind2 = np.where( (end_date >= file_date_range[1]) & (start_date <= file_date_range[1]) )[0]
+        #keep_ind2 = np.where( (start_date >= file_date_range[0]) & (start_date <= file_date_range[1]) )[0]
+        keep_ind  = np.sort(np.unique(np.append(keep_ind1,keep_ind2)))
         
+        if (len(keep_ind) == 0): 
+                raise ValueError("No netcdf files within requested date range")
+        else:
+                netcdfs = np.array(netcdfs)[keep_ind]
+    
+    return netcdfs 
