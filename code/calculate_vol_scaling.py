e6b61e61f13232f13487f2d514b07d83768cf718
Emma Boland <emmomp@bas.ac.uk>
2022-08-09 12:41:17 +0100

    Adding zonal width scaling to OHC_yz trends

--- code/calculate_vol_scaling.py
+++ code/calculate_vol_scaling.py
@@ -3,13 +3,13 @@
 """
 calculate_vol_scaling.py
 
-Code to calculate the volume of the HegGEM3-GC3.1-LL model by basin and depth bin.
+Code to calculate the volume of the HegGEM3-GC3.1-LL model by basin and depth bin, and the zonal width of each basin.
 
 For the SMURPHS ensemble, see (See Dittus et al. 2020 https://doi.org/10.1029/2019GL085806) and for the CMIP6 historical ensemble, Andrews et al. (2020) https://doi.org/10.1029/2019MS001995.
 
 See https://github.com/emmomp/SMURPHS_OHC for details
 
-Created in Jun 2022
+Updated August 2022
 
 @author: emmomp@bas.ac.uk Emma J D Boland
 """
@@ -30,8 +30,12 @@ depthbins = [0,300,700,2000,6001]
 masks=xr.open_dataset(data_dir+'other_model_data/subbasins_eORCA1-GO6-Daley.nc')
 griddata = xr.open_dataset(data_dir+'other_model_data/nemo_grid-T.nc')
 vol=(griddata.area*griddata.thkcello).squeeze()
+griddata2 = xr.open_dataset(data_dir+'other_model_data/mesh_mask_eORCA1-GO6.nc')
+dx=griddata2.e1t*griddata2.tmask
+dx=dx.where(dx>0)
+
 basin_masks={
-        'global':xr.full_like(vol,True),
+        'global':xr.full_like(griddata.area,True),
         'so':(vol.nav_lat<=-35.0),
         'atl':masks.tmaskatl.astype(bool),
         'ind':masks.tmaskind.astype(bool),
@@ -40,12 +44,16 @@ basin_masks={
 
 # Calculate volume by basin and depth range for scaling
 basin_vol=[]
+basin_width=[]
 for basin in basins:
     vol_fulld=vol.where(basin_masks[basin]).sum()
     vol_fulld['basin']=basin
     vol_fulld['deptht_bins']='Full Depth'
     vol_binned = vol.where(basin_masks[basin]).groupby_bins(vol.deptht,depthbins,labels=depthlabels).sum(['y','x','deptht'])    
     basin_vol.append(xr.concat([vol_fulld,vol_binned],'deptht_bins'))
+    widthx=dx.where(basin_masks[basin]).sum(dim='x')
+    widthx['basin']=basin
+    basin_width.append(widthx)    
 basin_vol=xr.concat(basin_vol,'basin')
 basin_vol.name='basin_volume'
 basin_vol.attrs['units']='m^3'
@@ -59,5 +67,14 @@ surface_area.attrs['long_name']='Ocean Surface Area'
 scalings = xr.merge([basin_vol,surface_area],combine_attrs="drop_conflicts")
 scalings.attrs.update(attrs)
 scalings.to_netcdf(save_dir+'other_model_data/vol_scalings.nc')
+
+basin_width=xr.concat(basin_width,'basin')
+ny=basin_width['y'].size
+basin_width=basin_width.rename({'z':'deptht'}).isel(y=slice(1,ny-1))
+basin_width.attrs['units']='m'
+basin_width.attrs['long_name']='Basin Zonal Width'
+basin_width.attrs['description']='Zonally summed x-width by depth, latitude and basin'
+basin_width.attrs.update(attrs)
+basin_width.to_netcdf(save_dir+'other_model_data/width_scalings.nc')
 
 print('Finished')
