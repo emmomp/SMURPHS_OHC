395e4fc9a1815de8c83005e84b534425abbc2669
emmomp <emmomp@bas.ac.uk>
2022-11-08 16:20:53 +0000

    Deptht->lev, time_c..-> time, new exp names

--- code/ohc_weightedtrends.py
+++ code/ohc_weightedtrends.py
@@ -27,33 +27,37 @@ attrs={'contact':'emmomp@bas.ac.uk',
        'date':'Created on '+date.today().strftime("%d/%m/%Y"),
        'notes':'Data produced by analysis of the SMURPHS ensemble, See Dittus et al. 2020 https://doi.org/10.1029/2019GL085806'}
 
-exps=['historical0p2','historical0p4','historical0p7','historical1p0','historical1p5']
+exps=['hist-0p2','hist-0p4','hist-0p7','hist-1p0','hist-1p5']
+
+volcello=xr.open_dataset(data_dir+'/volcello/gn/v20190628/volcello_Omon_HadGEM3-GC31-LL_piControl_r1i1p1f1_gn_185001-189912.nc')
+vol=volcello['volcello'][0]
 
 # Open model ohc time series
 ohc_global=[]
 for exp in exps:
     ohc_global.append(xr.open_mfdataset(save_dir+'ohc_tseries/ohc_bydepth_'+exp+'*global*.nc',concat_dim='run',combine='nested',data_vars=['ohc',]))
 ohc_global=xr.combine_nested(ohc_global,concat_dim='exp',data_vars=['ohc',]) 
-ohc_global=ohc_global.swap_dims({'time_counter':'time_centered'})
-ohc_global['time_mths']=('time_centered',np.arange(0,1980))
+ohc_global['time_mths']=('time',np.arange(0,1980))
 
 # Open drift from PIC experiment
 ohc_pic_drift=xr.open_dataarray(save_dir+'pic_data/ohc_pic_all_drift.nc')
-ohc_pic_drift['deptht_bins']=(('lev_bins',),[b'Full Depth',]+list(ohc_global['deptht_bins'].data))
-ohc_pic_drift=ohc_pic_drift.swap_dims({'lev_bins':'deptht_bins'})
-time_months=xr.DataArray(dims=['time_centered',],data=np.arange(0,1980))
+time_months=xr.DataArray(dims=['time',],data=np.arange(0,1980))
 
 # Remove drift
 ohc_global['ohc']=(ohc_global['ohc']-(ohc_pic_drift.sel(basin='global')*1e18*time_months))/1e22
 
 # Create two summed ranges
-ohc_0700=ohc_global.ohc.sel(deptht_bins=['0-300m','300-700m']).sum(dim='deptht_bins')
-ohc_02000=ohc_global.ohc.sel(deptht_bins=['0-300m','300-700m','700-2000m']).sum(dim='deptht_bins')
+ohc_0700=ohc_global.ohc.sel(lev_bins=['0-300m','300-700m']).sum(dim='lev_bins')
+ohc_02000=ohc_global.ohc.sel(lev_bins=['0-300m','300-700m','700-2000m']).sum(dim='lev_bins')
+
+# Multiply by vols to retrieve total OHC
+ohc_0700=ohc_0700*volcello.sel(lev=slice(None,700)).sum()
+ohc_02000=ohc_02000*volcello.sel(lev=slice(None,2000)).sum()
 
 # Linear fit to 1955-2015
-long_fit_0700=utils.lin_regress(ohc_global.time_mths.sel(time_centered=slice('1955-01-01',None)),ohc_0700.sel(time_centered=slice('1955-01-01',None)),[["time_centered"], ["time_centered"]])
+long_fit_0700=utils.lin_regress(ohc_global.time_mths.sel(time=slice('1955-01-01',None)),ohc_0700.sel(time=slice('1955-01-01',None)),[["time"], ["time"]])
 long_fit_0700['drange']='OHC0-700m'
-long_fit_02000=utils.lin_regress(ohc_global.time_mths.sel(time_centered=slice('1955-01-01',None)),ohc_02000.sel(time_centered=slice('1955-01-01',None)),[["time_centered"], ["time_centered"]])
+long_fit_02000=utils.lin_regress(ohc_global.time_mths.sel(time=slice('1955-01-01',None)),ohc_02000.sel(time=slice('1955-01-01',None)),[["time"], ["time"]])
 long_fit_02000['drange']='OHC0-2000m'
 long_fit=xr.concat([long_fit_0700,long_fit_02000],'drange')
 
@@ -67,7 +71,7 @@ long_fit.to_netcdf(save_dir+'ohc_trends/longfit_model.nc')
 
 all_fit=[]
 for tchunk in range(0,1608):
-    lin_fit=utils.lin_regress(ohc_global.time_mths[tchunk:tchunk+12*31],ohc_0700.isel(time_centered=slice(tchunk,tchunk+12*31)),[["time_centered"], ["time_centered"]])
+    lin_fit=utils.lin_regress(ohc_global.time_mths[tchunk:tchunk+12*31],ohc_0700.isel(time=slice(tchunk,tchunk+12*31)),[["time"], ["time"]])
     lin_fit=lin_fit.assign_coords(time_mths=tchunk)
     all_fit.append(lin_fit)
 all_fits_0700=xr.combine_nested(all_fit,concat_dim='time_mths')
@@ -77,7 +81,7 @@ all_fits_0700['drange']='OHC0-700m'
 
 all_fit=[]
 for tchunk in range(0,1608):
-    lin_fit=utils.lin_regress(ohc_global.time_mths[tchunk:tchunk+12*31],ohc_02000.isel(time_centered=slice(tchunk,tchunk+12*31)),[["time_centered"], ["time_centered"]])
+    lin_fit=utils.lin_regress(ohc_global.time_mths[tchunk:tchunk+12*31],ohc_02000.isel(time=slice(tchunk,tchunk+12*31)),[["time"], ["time"]])
     lin_fit=lin_fit.assign_coords(time_mths=tchunk)
     all_fit.append(lin_fit)
 all_fits_02000=xr.combine_nested(all_fit,concat_dim='time_mths')
