1a7b929907b57a2e195bc67a7ecd1c7428793d63
emmomp <emmomp@bas.ac.uk>
2022-11-08 11:30:46 +0000

    Fix indentation

--- code/ohc_by_basin_depth.py
+++ code/ohc_by_basin_depth.py
@@ -66,47 +66,47 @@ runs=['r1i1p1f2',' r2i1p1f2','r3i1p1f2','r4i1p1f2','r5i1p1f2']
 for exp in exps:
     for run in runs:
         files = glob.glob('{}/{}/{}/Omon/thetao/gn/v20190213/thetao_*.nc'.format(data_dir,exp,run))
-            #Initialise lists to hold data
-            ohc_datasets={}
-            ohc_bydepth_datasets={}       
+        #Initialise lists to hold data
+        ohc_datasets={}
+        ohc_bydepth_datasets={}       
 
-            with xr.open_mfdataset(files,concat_dim='time') as data:
-                print('Got data, calculating ohc')
-                data_weighted = data['thetao']*vol
+        with xr.open_mfdataset(files,concat_dim='time') as data:
+            print('Got data, calculating ohc')
+            data_weighted = data['thetao']*vol
 
-                for basin in basin_masks.keys():
-                    print(basin)
-                    with dask.config.set(**{'array.slicing.split_large_chunks': True}):
-                        data_masked = data_weighted.where(basin_masks[basin])
-                        ohc_datasets[basin]=data_masked.sum(dim=['i','j','lev'])*rho_0*c_p
-
-                        data_masked_binned = data_masked.groupby_bins(data.lev,depthbins,labels=depthlabels)
-                        ohc_bydepth_datasets[basin]=data_masked_binned.sum(dim=['i','j','lev'])*rho_0*c_p            
-                        
-            print('Combining, loading, and writing to file')
             for basin in basin_masks.keys():
                 print(basin)
-                ohc=ohc_datasets[basin]
-                ohc.load()
-                ohc.name='ohc'
-                ohc.attrs['long_name']=basin_longname[basin]+' depth integrated ocean heat content'
-                ohc.attrs['units']='J'
-                ohc['basin']=basin
-                ohc['exp']=exp
-                ohc['run']=run
-                ohc.attrs.update(attrs)
-                ohc.to_netcdf(save_dir+'ohc_tseries/ohc_'+exp+'_'+run+'_'+basin+'.nc')
-                
-                ohc_bydepth=ohc_bydepth_datasets[basin]
-                ohc_bydepth.load()         
-                ohc_bydepth.name='ohc'
-                ohc_bydepth.attrs['long_name']=basin_longname[basin]+' heat content by depth bin'
-                ohc_bydepth.attrs['units']='J'   
-                ohc_bydepth['basin']=basin
-                ohc_bydepth['exp']=exp
-                ohc_bydepth['run']=run      
-                ohc_bydepth.attrs.update(attrs)       
-                ohc_bydepth.to_netcdf(save_dir+'ohc_tseries/ohc_bydepth_'+exp+'_'+run+'_'+basin+'.nc')                
+                with dask.config.set(**{'array.slicing.split_large_chunks': True}):
+                    data_masked = data_weighted.where(basin_masks[basin])
+                    ohc_datasets[basin]=data_masked.sum(dim=['i','j','lev'])*rho_0*c_p
+
+                    data_masked_binned = data_masked.groupby_bins(data.lev,depthbins,labels=depthlabels)
+                    ohc_bydepth_datasets[basin]=data_masked_binned.sum(dim=['i','j','lev'])*rho_0*c_p            
+
+        print('Combining, loading, and writing to file')
+        for basin in basin_masks.keys():
+            print(basin)
+            ohc=ohc_datasets[basin]
+            ohc.load()
+            ohc.name='ohc'
+            ohc.attrs['long_name']=basin_longname[basin]+' depth integrated ocean heat content'
+            ohc.attrs['units']='J'
+            ohc['basin']=basin
+            ohc['exp']=exp
+            ohc['run']=run
+            ohc.attrs.update(attrs)
+            ohc.to_netcdf(save_dir+'ohc_tseries/ohc_'+exp+'_'+run+'_'+basin+'.nc')
+
+            ohc_bydepth=ohc_bydepth_datasets[basin]
+            ohc_bydepth.load()         
+            ohc_bydepth.name='ohc'
+            ohc_bydepth.attrs['long_name']=basin_longname[basin]+' heat content by depth bin'
+            ohc_bydepth.attrs['units']='J'   
+            ohc_bydepth['basin']=basin
+            ohc_bydepth['exp']=exp
+            ohc_bydepth['run']=run      
+            ohc_bydepth.attrs.update(attrs)       
+            ohc_bydepth.to_netcdf(save_dir+'ohc_tseries/ohc_bydepth_'+exp+'_'+run+'_'+basin+'.nc')                
             
             print('Done '+exp+' '+run)
          
