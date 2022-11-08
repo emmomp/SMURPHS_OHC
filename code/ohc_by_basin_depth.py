9c9075ff2c2cf9551c80070a1e1c84263a40bd44
emmomp <emmomp@bas.ac.uk>
2022-11-08 11:28:45 +0000

    Update to CMOR-ised data format

--- code/ohc_by_basin_depth.py
+++ code/ohc_by_basin_depth.py
@@ -17,11 +17,9 @@ Updated Nov 2022
 @author: emmomp@bas.ac.uk
 """
 
-import baspy as bp
 import xarray as xr
 import glob
-import cftime
-from datetime import date
+import dask
 
 rho_0 = 1.027e3 # Standard density
 c_p = 3850 # Heat capacity of sea water
@@ -33,10 +31,14 @@ attrs={'contact':'emmomp@bas.ac.uk',
 
 save_dir = '../data_in/' #Directory to save data to
 
+data_dir = '/gws/nopw/j04/smurphs/E/adittus/MASS_download/ocean/data_andrea/'
+
 masks=xr.open_dataset(save_dir+'other_model_data/subbasins_eORCA1-GO6-Daley.nc')
-griddata = xr.open_dataset(save_dir+'other_model_data/nemo_grid-T.nc')
-vol = (griddata.area*griddata.thkcello).squeeze()
-del vol['time_centered']
+# Match up grid formats by removing NEMO halo
+masks=masks.isel(x=slice(1,-1),y=slice(1,-1)) 
+masks=masks.rename({'x':'i','y':'j'})
+volcello=xr.open_dataset('/badc/cmip6/data/CMIP6/CMIP/MOHC/HadGEM3-GC31-LL/historical/r1i1p1f3/Omon/volcello/gn/latest/volcello_Omon_HadGEM3-GC31-LL_historical_r1i1p1f3_gn_185001-189912.nc')
+vol=volcello['volcello'][0]
 
 depthlabels=['0-300m','300-700m','700-2000m','2km+']
 depthbins = [0,300,700,2000,6001]
@@ -58,81 +60,33 @@ basin_longname={
         'pac':'Pacific',
         }
 
-# Generate dataframe with all temp data
-df = bp.catalogue(dataset='smurphs',Var=['thetao'])
-nt = len(bp.get_files(df.iloc[0]))
+exps=['hist-0p2','hist-0p4','hist-0p7','hist-1p0','hist-1p5']
+runs=['r1i1p1f2',' r2i1p1f2','r3i1p1f2','r4i1p1f2','r5i1p1f2']
 
-exps = set(df.Experiment)
-runs = set(df.RunID)
-                
-def preproc_data(ds):
-    return ds.drop('deptht_bounds')
-
-for index, row in df.iterrows():
-    exp = row[0]
-    run = row[1]
-    files = glob.glob(save_dir+'ohc_tseries/ohc_*'+exp+'_'+run+'*nc')
-    # Check if output already exists
-    if len(files)==10:
-        print('Skipping '+exp+' '+run)
-    else:        
-        print('Opening row '+exp+' '+run)
-        files = bp.get_files(row)
-        print('Found ',len(files),' files')
-        if len(files)==1980:
-            # Split file list into slices of 50 to stop massive slowdown
-            nf=50
-            file_slices=[files[i:i + nf] for i in range(0, len(files), nf)]
+for exp in exps:
+    for run in runs:
+        files = glob.glob('{}/{}/{}/Omon/thetao/gn/v20190213/thetao_*.nc'.format(data_dir,exp,run))
             #Initialise lists to hold data
             ohc_datasets={}
-            ohc_bydepth_datasets={}
-            for basin in basin_masks.keys():
-                ohc_datasets[basin]=[]
-                ohc_bydepth_datasets[basin]=[]
-            
-            for a,afiles in enumerate(file_slices):
-                print('Slice ',a)
-                try: 
-                    with xr.open_mfdataset(afiles,preprocess=preproc_data,concat_dim='time_counter',combine='combine_nested') as data:
-                        print('Got data, calculating ohc')
-                        data_weighted = data*vol
-                    
-                        for basin in basin_masks.keys():
-                            print(basin)
-                            data_masked = data_weighted.where(basin_masks[basin])
-                            ohc_datasets[basin].append(data_masked.thetao.sum(dim=['x','y','deptht'])*rho_0*c_p)
-                            
-                            data_masked_binned = data_masked.thetao.groupby_bins(data.deptht,depthbins,labels=depthlabels)
-                            ohc_bydepth_datasets[basin].append(data_masked_binned.sum(dim=['x','y','deptht'])*rho_0*c_p)  
+            ohc_bydepth_datasets={}       
 
-                except: #Some of the coords aren't correct in some files
-                    print('Reading files individually')
-                    for file in afiles:
-                        with xr.open_dataset(file) as data:
-                            data=data.drop('deptht_bounds')
-                            if file==afiles[0]:
-                                deptht=data['deptht']
-                            if data['deptht'][0]>10:
-                                data['deptht']=deptht
-                                lastdate=ohc_datasets[basin][-1].time_centered.compute().item()
-                                data['time_centered']=cftime.Datetime360Day(lastdate.year,lastdate.month+1,lastdate.day)
-                            print(file)
-                            print('Got data, calculating ohc')
-                            data_weighted = data*vol
-                    
-                            for basin in basin_masks.keys():
-                                print(basin)
-                                data_masked = data_weighted.where(basin_masks[basin])
-                                ohc_datasets[basin].append(data_masked.thetao.sum(dim=['x','y','deptht'])*rho_0*c_p)
-                                
-                                data_masked_binned = data_masked.thetao.groupby_bins(data.deptht,depthbins,labels=depthlabels)
-                                ohc_bydepth_datasets[basin].append(data_masked_binned.sum(dim=['x','y','deptht'])*rho_0*c_p)  
+            with xr.open_mfdataset(files,concat_dim='time') as data:
+                print('Got data, calculating ohc')
+                data_weighted = data['thetao']*vol
 
+                for basin in basin_masks.keys():
+                    print(basin)
+                    with dask.config.set(**{'array.slicing.split_large_chunks': True}):
+                        data_masked = data_weighted.where(basin_masks[basin])
+                        ohc_datasets[basin]=data_masked.sum(dim=['i','j','lev'])*rho_0*c_p
+
+                        data_masked_binned = data_masked.groupby_bins(data.lev,depthbins,labels=depthlabels)
+                        ohc_bydepth_datasets[basin]=data_masked_binned.sum(dim=['i','j','lev'])*rho_0*c_p            
                         
             print('Combining, loading, and writing to file')
             for basin in basin_masks.keys():
                 print(basin)
-                ohc=xr.concat(ohc_datasets[basin],'time_counter')
+                ohc=ohc_datasets[basin]
                 ohc.load()
                 ohc.name='ohc'
                 ohc.attrs['long_name']=basin_longname[basin]+' depth integrated ocean heat content'
@@ -143,7 +97,7 @@ for index, row in df.iterrows():
                 ohc.attrs.update(attrs)
                 ohc.to_netcdf(save_dir+'ohc_tseries/ohc_'+exp+'_'+run+'_'+basin+'.nc')
                 
-                ohc_bydepth=xr.concat(ohc_bydepth_datasets[basin],'time_counter')
+                ohc_bydepth=ohc_bydepth_datasets[basin]
                 ohc_bydepth.load()         
                 ohc_bydepth.name='ohc'
                 ohc_bydepth.attrs['long_name']=basin_longname[basin]+' heat content by depth bin'
@@ -155,7 +109,5 @@ for index, row in df.iterrows():
                 ohc_bydepth.to_netcdf(save_dir+'ohc_tseries/ohc_bydepth_'+exp+'_'+run+'_'+basin+'.nc')                
             
             print('Done '+exp+' '+run)
-        else: 
-            print('Skipping '+exp+' '+run)
          
 print('All Done')
