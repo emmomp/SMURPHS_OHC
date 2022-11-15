572c92ed84be1d914ad9e51e764d46ef88ecda8b
emmomp <emmomp@bas.ac.uk>
2022-11-15 11:47:47 +0000

    Update trend code

--- code/ohc_xy_trends.py
+++ code/ohc_xy_trends.py
@@ -31,8 +31,8 @@ save_dir = '../data_in/' #Directory to save data to
 exps=['hist-0p2','hist-0p4','hist-0p7','hist-1p0','hist-1p5']
 runs=['r1i1p1f2','r2i1p1f2','r3i1p1f2','r4i1p1f2','r5i1p1f2']
 
-startdate='1955-01-01'
-enddate='2015-01-01'
+startdate=19550101
+enddate=20141231
 
 trend_startdates=['1960-01-16','1980-01-16']
 trend_enddates=['1990-12-16','2010-12-16']
--- code/ohc_yz_trends.py
+++ code/ohc_yz_trends.py
@@ -28,34 +28,16 @@ attrs={'contact':'emmomp@bas.ac.uk',
 
 save_dir = '../data_in/' #Directory to save data to
 
-exps=['historical0p2','historical0p4','historical0p7','historical1p0','historical1p5']
-runs=['r1i1p1f1','r2i1p1f1','r3i1p1f1','r4i1p1f1','r5i1p1f1']
-
-masks=xr.open_dataset(save_dir+'other_model_data/subbasins_eORCA1-GO6-Daley.nc')
-
-basin_masks={
-        'so':(masks.nav_lat<=-35.0),
-        'atl':masks.tmaskatl.astype(bool),
-        'ind':masks.tmaskind.astype(bool),
-        'pac':masks.tmaskpac.astype(bool)
-            }
-
-basins=list(basin_masks.keys())
-basins=basins+['global']
+exps=['hist-0p2','hist-0p4','hist-0p7','hist-1p0','hist-1p5']
+runs=['r1i1p1f2','r2i1p1f2','r3i1p1f2','r4i1p1f2','r5i1p1f2']
+basins=['global','so','pac','atl','ind']
 
 startdate=19550101
 enddate=20141231
 
-startdate_str='1955-01-01'
-enddate_str='2014-12-30'
-
 trend_startdates=['1960-01-16','1980-01-16']
 trend_enddates=['1990-12-16','2010-12-16']
 trend_middates=['1975-06-16','1995-06-16']
-
-y_start=[1960,1980]
-y_end=[1991,2011]
-y_mid=[1975.5,1995.5]
  
 foo=[]
 for ie,exp in enumerate(exps):
@@ -65,7 +47,7 @@ for ie,exp in enumerate(exps):
         ds['basin']=(('basin'),[basin,])
         foo[ie].append(ds)
 ohc_yz=xr.combine_nested(foo,concat_dim=['exp','basin']) 
-ohc_yz['time_mths']=('time_centered',np.arange(0,721)) 
+ohc_yz['time_mths']=('time',np.arange(0,721)) 
 
 print('ohc data loaded, running linear regressions')
 
@@ -74,14 +56,14 @@ all_fit=[]
 for tchunk in range(0,2):
     #fit all runs
     tslice=slice(trend_startdates[tchunk],trend_enddates[tchunk])
-    xx=ohc_yz['time_mths'].sel(time_centered=tslice)
-    yy=ohc_yz['ohc'].sel(time_centered=tslice)
-    lin_fit=utils.lin_regress(xx,yy,[["time_centered"], ["time_centered"]])
+    xx=ohc_yz['time_mths'].sel(time=tslice)
+    yy=ohc_yz['ohc'].sel(time=tslice)
+    lin_fit=utils.lin_regress(xx,yy,[["time"], ["time"]])
     lin_fit=lin_fit.assign_coords(time=trend_middates[tchunk])
     run_fit.append(lin_fit)
     #fit run mean
     yy=yy.mean(dim='run')
-    lin_fit=utils.lin_regress(xx,yy,[["time_centered"], ["time_centered"]])
+    lin_fit=utils.lin_regress(xx,yy,[["time"], ["time"]])
     lin_fit=lin_fit.assign_coords(time=trend_middates[tchunk])
     all_fit.append(lin_fit)
     
@@ -92,9 +74,8 @@ all_fit=xr.concat(all_fit,'time')
 drift=xr.open_dataarray(save_dir+'pic_data/ohc_yz_pic_drift.nc')
 
 # Remove drift
-ny=run_fit['y'].size
-run_slope= run_fit.isel(y=slice(1,ny-1),parameter=0)-drift
-all_slope=all_fit.isel(y=slice(1,ny-1),parameter=0)-drift
+run_slope= run_fit-drift
+all_slope=all_fit-drift
 
 print('regressions finished, writing to file')
 all_slope.attrs.update(attrs)
